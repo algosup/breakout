a3402965002513de844526b5c7aaf2916ac052d6
Franck JEANNIN <franck.jeannin@algosup.com>
2020-10-14 14:01:29 +0200

    First release

---
+++ breakout.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"github.com/algosup/game"
+	"github.com/algosup/game/color"
+	"github.com/algosup/game/key"
+)
+
+var ballLeft = 250
+var ballTop = 400
+var deltaX = 1
+var deltaY = 1
+var padLeft = 200
+
+const ballSize = 10
+const width = brickWidth * 23
+const height = 500
+const padWidth = 70
+const padHeight = 5
+const padTop = height - padHeight*2
+const brickWidth = 30
+const brickHeight = 10
+
+var colors = []color.Color{
+	color.Black,
+	color.Red,
+	color.Blue,
+	color.Green,
+}
+
+var bricks = [][]uint8{
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+	{0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+	{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+	{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2},
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 1, 2},
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2},
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2},
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2},
+	{2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2},
+}
+
+func drawBricks(surface game.Surface) {
+	for row := 0; row < len(bricks); row++ {
+		for col := 0; col < len(bricks[row]); col++ {
+			v := bricks[row][col]
+			if v > 0 {
+				game.DrawRect(surface, col*brickWidth, row*brickHeight, brickWidth-1, brickHeight-1, colors[v])
+			}
+		}
+	}
+}
+
+func bounce() {
+	newDeltaX := deltaX
+	newDeltaY := deltaY
+	for row := 0; row < len(bricks); row++ {
+		for col := 0; col < len(bricks[row]); col++ {
+			v := bricks[row][col]
+			if v > 0 {
+				left := col * brickWidth
+				top := row * brickHeight
+				right := left + brickWidth
+				bottom := top + brickHeight
+				w := intersect(ballLeft, ballLeft+ballSize, left, right)
+				h := intersect(ballTop, ballTop+ballSize, top, bottom)
+
+				if w > 0 && h > 0 {
+					bricks[row][col] = 0
+					if w >= h {
+						newDeltaY = -deltaY
+					}
+					if h >= w {
+						newDeltaX = -deltaX
+					}
+				}
+			}
+		}
+	}
+
+	deltaX = newDeltaX
+	deltaY = newDeltaY
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func intersect(start1, end1, start2, end2 int) (v int) {
+	start := max(start1, start2)
+	end := min(end1, end2)
+	return max(0, end-start)
+}
+
+func draw(surface game.Surface) {
+	ballLeft += deltaX
+	ballTop += deltaY
+
+	bounce()
+
+	if ballLeft == 0 {
+		deltaX = -deltaX
+	}
+	if ballLeft+ballSize == width {
+		deltaX = -deltaX
+	}
+	if ballTop == 0 {
+		deltaY = -deltaY
+	}
+	if intersect(ballTop, ballTop+ballSize, padTop, padTop+padHeight) > 0 {
+		if intersect(ballLeft, ballLeft+ballSize, padLeft, padLeft+padWidth) > 0 {
+			if ballLeft+ballSize/2 < padLeft+padWidth/2 {
+				deltaX = -1
+			} else {
+				deltaX = 1
+			}
+
+			deltaY = -1
+		}
+	}
+
+	if ballTop == height {
+		ballLeft = padLeft + padWidth/2
+		ballTop = padTop - padHeight
+		deltaX = 1
+		deltaY = 1
+	}
+
+	if key.IsPressed(key.Left) {
+		if padLeft+padWidth/2 > 0 {
+			padLeft -= 3
+		}
+	}
+
+	if key.IsPressed(key.Right) {
+		if padLeft+padWidth/2 < width {
+			padLeft += 3
+		}
+	}
+
+	drawBricks(surface)
+	game.DrawRect(surface, padLeft, padTop, padWidth, padHeight, color.Orange)
+	game.DrawRect(surface, ballLeft, ballTop, ballSize, ballSize, color.White)
+}
+
+func main() {
+	game.Run("BREAKOUT", width, height, draw)
+}
